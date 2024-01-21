2998869c9bbf83c18955530eb187175010ae74e2
shobhitmaitrey <102179529+shobhitmaitrey@users.noreply.github.com>
2024-01-21 23:26:31 +0530

    Create calculateSimpleInterest.py

---
+++ calculateSimpleInterest.py
@@ -0,0 +1,9 @@
+# calculate Simple Interest without storing to a variable
+p=int(input("enter the principle amount="))
+r=int(input("enter the rate="))
+t=int(input("enter the time="))
+print("simple interest =",(p*r*t)/100)
+
+# introducing variable to store SI
+si = (p*r*t)/100;
+print("si =", si);
