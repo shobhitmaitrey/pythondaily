a32ad109e36e5d44293d8ed96f2a6e62e7af7566
shobhitmaitrey <102179529+shobhitmaitrey@users.noreply.github.com>
2024-01-23 23:24:50 +0530

    Create day11.py

---
+++ day11.py
@@ -0,0 +1,19 @@
+# type() function
+a = 6
+print(type(a))
+
+name = "peter"
+print(type(name))
+
+val = 0.2
+print(type(val))
+
+
+
+```
+output
+<class 'int'>
+<class 'str'>
+<class 'float'>
+
+```
