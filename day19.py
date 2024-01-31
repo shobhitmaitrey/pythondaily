61afeb156754419a3c5ea7dcd78ed88218ff682d
shobhitmaitrey <102179529+shobhitmaitrey@users.noreply.github.com>
2024-01-31 21:05:45 +0530

    Create day19.py

---
+++ day19.py
@@ -0,0 +1,8 @@
+# take input number from user and check wether number is greater than 5 or not
+
+int num = int(input("Enter a Value: "))
+
+if (num > 5):
+  print("number is greater than 5")
+else :
+  print("number is greater than 5")
