329477fc40ce1977e00f928c007516cdf22fcf33
shobhitmaitrey <102179529+shobhitmaitrey@users.noreply.github.com>
2024-02-01 21:55:28 +0530

    Create day20.py

---
+++ day20.py
@@ -0,0 +1,10 @@
+# check number input number is greater than 5, equal to 5 or less than 5
+
+a = int(input("Enter a number: "))
+
+if(a>5):
+  print("Greater Than 5")
+elif(a==5):
+  print("Equal To 5")
+else:
+  print("Less Than 5")
